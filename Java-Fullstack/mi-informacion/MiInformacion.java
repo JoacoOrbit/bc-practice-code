7b1e755d30ff7f95a817cdbc939224e78c762685
JoacoOrbit <88943020+JoacoOrbit@users.noreply.github.com>
2025-08-26 17:00:03 -0400

    adds java basic exercises

---
+++ Java-Fullstack/mi-informacion/MiInformacion.java
@@ -0,0 +1,8 @@
+public class MiInformacion {
+    public static void main(String [] args){
+        System.out.println("Hola, mi nombre es Joaquín Araya");
+        System.out.println("Tengo 24 años de edad");
+        System.out.println("Soy de Chile y vivo en la ciudad de Ovalle");
+        System.out.println("Me gusta mucho jugar y ver anime");
+    }
+}
