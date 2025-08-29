94fba6b617925c452e20f6e4a94c5a32c5cc3786
JoacoOrbit <88943020+JoacoOrbit@users.noreply.github.com>
2025-08-29 15:43:44 -0400

    finishes atm exercise

--- Java-Fullstack/cajero/App.java
+++ Java-Fullstack/cajero/App.java
@@ -2,6 +2,13 @@ import clases.Cajero;
 
 public class App {
     public static void main (String args []){
-
+        System.out.println(Cajero.retirarDinero(5000));
+        System.out.println(Cajero.retirarDinero(9000));
+        System.out.println(Cajero.pagarFactura(2000, 2000));
+        System.out.println(Cajero.pagarFactura(2000, 2500));
+        System.out.println(Cajero.pagarFactura(3000, 2500));
+        System.out.println(Cajero.cambiarBilletes(5000));
+        System.out.println(Cajero.cambiarBilletes(5010));
+        System.out.println(Cajero.totalDeBilletesYMonedas(16243));
     }
 }
--- Java-Fullstack/cajero/clases/Cajero.java
+++ Java-Fullstack/cajero/clases/Cajero.java
@@ -22,7 +22,7 @@ public class Cajero {
         }
     }
 
-    public static boolean cambiarBilletes(int ingresa, int resultado){
+    public static boolean cambiarBilletes(int ingresa){
         if (ingresa % 50 == 0){
             return true;
         } else {
@@ -31,6 +31,35 @@ public class Cajero {
     }
 
     public static String totalDeBilletesYMonedas (int monto) {
-        return "";
+        int b500;
+        int b200;
+        int b100;
+        int b50;
+        int b20;
+        int m10;
+        int m5;
+        int m2;
+        int m1;
+        int sobra;
+
+        b500 = monto / 500;
+        sobra = monto % 500;
+        b200 = sobra / 200;
+        sobra = sobra % 200;
+        b100 = sobra / 100;
+        sobra = sobra % 100;
+        b50 = sobra / 50;
+        sobra = sobra % 50;
+        b20 = sobra / 20;
+        sobra = sobra % 20;
+        m10 = sobra / 10;
+        sobra = sobra % 10;
+        m5 = sobra / 5;
+        sobra = sobra % 5;
+        m2 = sobra / 2;
+        sobra = sobra % 2;
+        m1 = sobra;
+
+        return "billetes de 500: " + b500 + ", billetes de 200: " + b200 + ", billetes de 100: " + b100 + ", billetes de 50: " + b50 + ", billetes de 20: " + b20 + ", monedas de 10: " + m10 + ", monedas de 5: " + m5 + ", monedas de 2: " + m2 + ", monedas de 1: " + m1;
     }
 }
